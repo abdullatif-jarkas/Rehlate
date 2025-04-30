df62dbd2c35bc0a4532c80815d955e1c383529e7
abdullatif <abdullatifjr002@gmail.com>
2025-04-30 10:30:25 +0300

    bug fixes

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Layout from "./Layout/Layout.tsx";
-import Home from "./pages/Home.tsx";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Layout>
-      <Home />
+      <App />
     </Layout>
   </StrictMode>
 );
