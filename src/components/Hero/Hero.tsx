15286fd4fcb617c2beb8cd2ee52603d44319aa89
haneen <haneenhariry8@gmail.com>
2025-04-30 10:46:24 +0300

    font

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,8 +1,8 @@
 
 export default function Hero() {
   return (
-    <div>
-      
+    <div className="bg-[url(/hero.jpg)] bg-cover bg-center font-lama h-screen">
+       <h1 className=" text-white text-shadow text-7xl font-bold">سافر معنا إلى العالم</h1>
     </div>
   )
 }
